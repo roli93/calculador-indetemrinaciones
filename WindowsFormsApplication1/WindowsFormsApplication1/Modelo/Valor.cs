a39a81dd6e97beb2fa33e5b2130b3d93461c7dca
Rodolfo Caputo <rodcap.93@gmail.com>
2015-04-19 01:55:55 -0300

    Commit inicial con lo que ya hay hecho

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Modelo/Valor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Modelo
+{
+    public class Valor
+    {
+        public double ValorRepresentativo { get; set; }
+        public double ErrorAbsoluto { get; set; }
+
+        public Valor() : base() { }
+
+        public Valor(string expresion)
+        {
+            parseExpresion(expresion);
+        }
+
+        public double ErrorRelativo
+        {
+            get
+            {
+                return ErrorAbsoluto / ValorRepresentativo;
+            }
+
+            set
+            {
+                ErrorAbsoluto = ValorRepresentativo * value;
+                //TODO Mejorar esto que queda dependiente del orden.
+            }
+        }
+
+
+        //TODO abstraer bien cosas comunes acá
+        private Valor operarAbsoluta(Valor otroValor, Func<double, double, double> mainOperation)
+        {
+            Valor valor =  new Valor();
+            valor.ValorRepresentativo = mainOperation(this.ValorRepresentativo, otroValor.ValorRepresentativo);
+            valor.ErrorAbsoluto = this.ErrorAbsoluto + otroValor.ErrorAbsoluto;
+            return valor;
+        }
+
+        private Valor operarRelativa(Valor otroValor, Func<double, double, double> mainOperation)
+        {
+            Valor valor = new Valor();
+            valor.ValorRepresentativo = mainOperation(this.ValorRepresentativo, otroValor.ValorRepresentativo);
+            valor.ErrorRelativo = this.ErrorRelativo + otroValor.ErrorRelativo;
+            return valor;
+        }
+
+        public Valor sumarle(Valor otroValor)
+        {
+            return operarAbsoluta(otroValor, (a, b) => { return a + b; });
+        }
+
+        public Valor restarle(Valor otroValor)
+        {
+            return operarAbsoluta(otroValor, (a, b) => { return a - b; });
+        }
+
+        public Valor multiplicarPor(Valor otroValor)
+        {
+            return operarRelativa(otroValor, (a, b) => { return a * b; });
+        }
+
+        public Valor dividirPor(Valor otroValor)
+        {
+            return operarRelativa(otroValor, (a, b) => { return a / b; });
+        }
+
+        private void parseExpresion(string expresion)
+        {
+            string[] split = new string[] {};
+
+            expresion.Where<char>(c=>c.Equals('±')).Single<char>();
+
+            split = expresion.Split("±".ToCharArray(), 2);
+
+            ValorRepresentativo = Convert.ToDouble(split[0]);
+            ErrorAbsoluto = Convert.ToDouble(split[1]);
+
+        }
+
+        public override string ToString()
+        {
+            return ValorRepresentativo.ToString() + " ± " + ErrorAbsoluto.ToString();
+        }
+
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/UI/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Modelo;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            operar(textBox1.Text, textBox2.Text, (a, b) => a.sumarle(b), label5); 
+        }
+
+        private void operar(string expresion1, string expresion2,Func<Valor,Valor,Valor> operacion, Label labelResultado)
+        {
+            try
+            {
+                Valor a = new Valor(expresion1);
+                Valor b = new Valor(expresion2);
+                labelResultado.Text = operacion(a,b).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Las expresiones no tienen el formato adecuado", "Error");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            operar(textBox5.Text, textBox6.Text, (a, b) => a.multiplicarPor(b), label7); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            operar(textBox3.Text, textBox4.Text, (a, b) => a.restarle(b), label1); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            operar(textBox7.Text, textBox8.Text, (a, b) => a.dividirPor(b), label10); 
+        }
+
+
+    }
+}
